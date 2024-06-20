af8d82103b5d4fda39e6434804ecfa2e0e49ca14
dtumen <dt1109@mail.ru>
2024-06-20 13:46:26 +0200

    feat: add Button.stories.tsx

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta } from '@storybook/react';
+
+import { Button } from './Button';
+import { useState } from 'react';
+
+const meta: Meta<typeof Button> = {
+    title: 'Stories/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+        backgrounds: {
+            values: [
+                { name: 'light', value: '#fff' },
+                { name: 'dark', value: '#333' },
+                { name: 'purple', value: '#7d24a4' },
+            ],
+        }
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Normal = () =>  {
+    const [isDisable, setDisable] = useState(false);
+    return (
+        <Button
+            name={'button'}
+            onClick={() => {}}
+            disabled={isDisable} />
+    )
+}
+
+export const Disabled = () => {
+    return (
+        <Button
+            name={'button'}
+            onClick={() => {}}
+            disabled />
+    )
+}
