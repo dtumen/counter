ed463474c8dbd15884147fd94aa18790f0ebb2dc
dtumen <dt1109@mail.ru>
2024-08-20 13:33:55 +0200

    feat: add Counter.stories.tsx
    - feat Default story

---
+++ src/components/Counter/Counter.stories.tsx
@@ -0,0 +1,59 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import Counter from './Counter';
+import {CurrentValueType, ValuesConfigType} from '../../common/types/types';
+import {useState} from 'react';
+
+const meta = {
+    title: 'Stories/Counter',
+    component: Counter,
+    parameters: {
+        layout: 'centered',
+        backgrounds: {
+            values: [
+                {name: 'light', value: '#fff'},
+                {name: 'dark', value: '#333'},
+                {name: 'purple', value: '#7d24a4'},
+            ],
+        }
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof Counter>;
+
+export default meta;
+
+export const Deafault = () => {
+    const [currentValue, setCurrentValue] = useState<CurrentValueType>(0);
+    const [defaultConfig, setDefaultConfig] = useState<ValuesConfigType>({
+        maxValue: 5,
+        startValue: 0,
+    })
+
+    const [isChange, setIsChange] = useState(false);
+
+    const increaseHandler = () => {
+        setCurrentValue((prevValue) => {
+            if (typeof prevValue === 'number') {
+                return prevValue + 1;
+            }
+
+            return prevValue;
+        })
+    };
+
+    const resetHandler = () => {
+        if (typeof currentValue === 'number') {
+            setCurrentValue(defaultConfig.startValue);
+        }
+    };
+
+    return (
+        <Counter
+            currentValue={currentValue}
+            defaultConfig={defaultConfig}
+            isChange={isChange}
+            onIncrease={increaseHandler}
+            onReset={resetHandler}
+        />
+    )
+}
